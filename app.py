d986db000c33f3d35fd852dc0a47fd7ac21ed93c
Karmen Too <karmen.tanaka@gmail.com>
2022-10-12 11:09:23 -0400

    still not working

--- app.py
+++ app.py
@@ -11,25 +11,55 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS']= False
 
 boggle_game = Boggle()
 
+
 @app.route('/')
 def home():
-    the_board=generate_board()
-    session['board'] = the_board
-    return render_template('home.html', the_board=the_board)
+    return render_template('index.html')
 
 def generate_board():
     the_board = boggle_game.make_board()
     return the_board
 
-@app.route('/check_word', methods=['POST'])
+@app.route('/startGame')
+def start():
+    the_board=generate_board()
+    session['board'] = the_board
+    return render_template('home.html', the_board=the_board)
+
+@app.route('/check_word')
 def check_word():
-    print('*********')
-    print(request.form['guess'])
-    guess = request.form['guess']
-    if guess in boggle_game.words:
-        print('yeah')
-        result = "yeah"
+    the_board =session['board']
+    guess = request.args['guess']
+   
+    # check if it is a valid word
+    return jsonify({'result':boggle_game.check_valid_word(the_board, guess)})
+
+@app.route('/more_statistics', methods=["POST"])
+def more_stats():
+    score=request.json
+    print('*from request*')
+    print(score)
+    print('****')
+    print(session)
+    if 'score' in session:
+        print("****score is in session")
+        highest = session['score']
+        
     else:
-        print('oh nope')
-        result = "nope"
-    return result
+        print("****score is NOT in session")
+        session['score'] = 0
+        highest=0
+    print('*****highest socre in session *******')
+    print(highest)
+    session['score']=highest
+    if highest < score['score']:
+        session['score'] = score
+    attempts=session.get('attempt',0)
+    attempts+=1
+    session['attempt']=attempts
+    print('***score*****')
+    print(session['score'])
+    print('***attempt*****')
+    print(session['attempt'])
+    
+    return jsonify(session['score'])
--- static/app.js
+++ static/app.js
@@ -1,17 +1,62 @@
-//const axios = require('axios');
+
 BASE_URL = "http://127.0.0.1:5000"
+let score = 0
 
-$("#submitGuess").on("click", () => sendData, $(this).val())
+$("#myForm").on("submit", sendData)
 
-async function sendData(word) {
-    alert('in heree')
-    const response = await axios({
-        url: `${BASE_URL}/check_word`,
-        method: "POST",
-        data: {
-            word: word
+setTimeout(function () {
+    $('#submitGuess').prop('disabled', true)
+    $('#guess').prop('disabled', true)
+    $('#timer').text('Time is up!')
+    //save the highest score in session and how many times they played
+    moreStatistics()
+}, 10000)
+
+async function sendData(e) {
+    e.preventDefault()
+    word = $("#guess").val()
+    console.log(word)
+    try {
+        const response = await axios({
+            url: `${BASE_URL}/check_word`,
+            method: "GET",
+            params: {
+                guess: word
+            }
+        });
+        console.log(response)
+
+        if (response.data.result == "ok") {
+            $("#result").text('Good guess!')
+            // add score
+            score += word.length
+
+        } else if (response.data.result == "not-on-board") {
+            $("#result").text('Doesn not exist on board!')
+        } else {
+            $("#result").text('Not a word!')
         }
-    });
-    console.debug(response)
-    return response
+        $("#score").text(`Your score is ${score}!`)
+    } catch (e) {
+        console.log(e)
+        console.log('something went wrong')
+    }
+}
+
+async function moreStatistics() {
+    try {
+        const response = await axios({
+            url: `${BASE_URL}/more_statistics`,
+            method: "POST",
+            data: {
+                score: score
+            }
+        });
+        console.log(response)
+
+
+    } catch (e) {
+        console.log(e)
+        console.log('something went wrong')
+    }
 }
